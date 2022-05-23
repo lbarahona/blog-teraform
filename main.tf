121f52828b94f980dc6a82ca4394956a22323155
Lester Barahona <lestermiller26@gmail.com>
2022-05-22 21:54:50 -0600

    Removed namespace management

--- main.tf
+++ main.tf
@@ -10,11 +10,3 @@ resource "digitalocean_kubernetes_cluster" "lbarahona" {
     node_count = 2
   }
 }
-
-#Blog namespace
-#Since the namespace won't be modified, we can create it once and reuse it
-resource "kubernetes_namespace" "lbarahona-blog" {
-  metadata {
-    name = "lbarahona-blog"
-  }
-}
