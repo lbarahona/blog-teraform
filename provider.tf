cf96d8d80dff5344cb3bf477b2d26352883ad8c4
Lester Barahona <lestermiller26@gmail.com>
2022-05-23 19:25:46 -0600

    Manage state with terraform cloud

--- provider.tf
+++ provider.tf
@@ -1,3 +1,14 @@
+# Configure terraform remote state {
+terraform {
+  cloud {
+    organization = "lbarahona"
+
+    workspaces {
+      name = "lbarahona-blog"
+    }
+  }
+}
+
 # Configure the DigitalOcean Provider
 provider "digitalocean" {
   token = var.digitalocean_token
