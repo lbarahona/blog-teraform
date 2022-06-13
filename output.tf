8d1518ed36c7f541cded139ccc026ffac02bed16
Lester Barahona <lestermiller26@gmail.com>
2022-06-13 16:21:48 -0600

    Add output for kubeconfig

--- output.tf
+++ output.tf
@@ -2,3 +2,8 @@ resource "local_file" "kubernetes_config" {
   content  = digitalocean_kubernetes_cluster.lbarahona.kube_config.0.raw_config
   filename = "kubeconfig.yaml"
 }
+
+output "kubeconfig" {
+  value = digitalocean_kubernetes_cluster.lbarahona.kube_config.0.raw_config
+  sensitive = true
+}
